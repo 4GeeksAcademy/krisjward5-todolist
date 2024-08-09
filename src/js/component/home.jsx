875a2ebd8ff8fbfddb24493fc9065ac9a12e458e
Krisjward5 <169105340+Krisjward5@users.noreply.github.com>
2024-08-09 16:21:56 +0000

    working

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ToDoContainer } from "./todocontainer";
 
 
 
 //create your first component
 const Home = () => {
-	
-const fetchUsers = () => {
-	fetch('https://playground.4geeks.com/todo/users/kris_ward')
-	.then((res)=>res.json())
-	.then((user)=>{
-		setToDos(user.todo)
-	})
-} 
+
+	const [toDos, setToDos] = useState([]);
+
+useEffect(() => {
+	fetchUsers()
+}, []);
+
+
+	const fetchUsers = () => {
+		fetch('https://playground.4geeks.com/todo/users/krisward')
+			.then((res) => res.json())
+			.then((user) => {
+
+				setToDos(user.todo)
+			})
+	}
 
 
 	return (
-		<ToDoContainer/>
+		<ToDoContainer />
 	);
 };
 
